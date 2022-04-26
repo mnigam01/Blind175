33f4790e4dcd84582907d75a258c1d7b7d1a1813
mnigam01 <38101061+mnigam01@users.noreply.github.com>
2022-04-26 16:14:33 +0530

    Update Best_Time_to_Buy_and_Sell_Stock.cpp

--- Best_Time_to_Buy_and_Sell_Stock.cpp
+++ Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -2,13 +2,19 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         
-        int ans = 0;
-        int prv = prices[0];
         int n = prices.size();
+        int ans = 0;
+        int buy = prices[0];
         for(int i=1;i<n;i++)
         {
-            prv = min(prv,prices[i]);
-            ans = max(ans,prices[i]-prv);
+            if (prices[i]>buy)
+            {
+                ans = max(ans,prices[i]-buy);
+            }
+            else if (prices[i]<buy)
+            {
+                buy = prices[i];
+            }
         }
         return ans;
         
